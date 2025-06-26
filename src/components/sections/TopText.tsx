6974647bd4852bc621acb028704ed9d58014172d
deepak <y2codedev@gmail.com>
2025-06-26 13:55:09 +0530

    add share com..

--- src/components/sections/TopText.tsx
+++ src/components/sections/TopText.tsx
@@ -17,7 +17,7 @@ const TopText = () => {
                 )}
                 {content?.title.split(content?.highlight || '')[1]}
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
+            <p className="mt-6 sm:text-lg text-sm leading-8 text-gray-600 dark:text-gray-300">
                 {content?.description}
             </p>
         </div>
